fb363a524c186b4fefcade1b11aecebca489cf3d
Kevin Townson <ktown5422@gmail.com>
2019-07-13 13:21:55 -0500

    setup routes and build blog class

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ktizzle314@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) 
 
 class Blog(db.Model):
 
