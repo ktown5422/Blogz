72c82fa711db37b4967dd0e455e130473d635811
Kevin Townson <ktown5422@gmail.com>
2019-07-13 13:18:17 -0500

    setup routes and build blog class

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ktizzle314@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    post = db.Column(db.String(120))
+
+    def __init__(self, title, post):
+        self.title = title
+        self.post = post
+
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    
+
+
+
+@app.route('/' methods=['POST'])
+def 
+
+
+if __name__ == '__main__':
+    app.run()
