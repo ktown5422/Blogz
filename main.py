b8ee3fc9b4d8fbb49989c4f2f624bc8408577c46
Kevin Townson <ktown5422@gmail.com>
2019-07-18 17:58:28 -0500

    updated routes and add templates

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    post = db.Column(db.String(120))
+    post = db.Column(db.String(db.text))
 
     def __init__(self, title, post):
         self.title = title
@@ -19,16 +19,37 @@ class Blog(db.Model):
 
 
 
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    
+    return redirect('/blog')
+
+@app.route('/blog', methods=['POST'])
+def blog():
+
+    return render_template('blog.html')
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def newpost():
+    if request.method == 'POST':
+        blog_title = request.form['blog_title']
+        blog_post = request.form['blog_post']
+        title_error=''
+        new_post_error=''
+        if not blog_title.strip():
+            title_error = 'Please insert title'
+        if not blog_post.strip():
+            new_post_error = 'Pleases insert a blog post'
+        if title_error or new_post_error:
+            return render_template('newpost.html', title_error=title_error, new_post_error=new_post_error)
+        else:
+            new_blog = Blog(blog_title, blog_post)
+            db.session.add(new_blog)
+            db.session.commit()
 
 
+           
 
-@app.route('/' methods=['POST'])
-def 
-
+    return render_template('newpost.html', blog_title=blog_title, blog_post=blog_post)    
 
 if __name__ == '__main__':
     app.run()
